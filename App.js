667c9533dee9f80e49064c5edb0039c362d5da9b
starlord <starlord@xhiftcorp.com>
2018-11-06 11:10:49 +0900

    loading test

--- App.js
+++ App.js
@@ -1,15 +1,53 @@
 import React from 'react';
+import { AppLoading, Asset, Font } from "expo";
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons, MaterialIcons } from "@expo/vector-icons"
 
 export default class App extends React.Component {
+
+  state = {
+    isLoadingComplete : false
+  }
   render() {
+    const { isLoadingComplete} = this.state;
+    if(!isLoadingComplete){
+      return (
+              <AppLoading
+                startAsync={this._loadAssetAsync}
+                onError={this._handleFinishError}
+                onFinish={this._handleFinishLoading}
+              />
+            )
+    }
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>아오!! Open up App.js to start working on your app!!!</Text>
       </View>
     );
   }
 }
+
+_loadAssetAsync = async() => {
+  return Promise.all([
+    Asset.loadAsync([
+      require("./assets/images/logo.png")
+    ]),
+    Font.loadAsync([
+      ...Ionicons.font,
+      ...MaterialIcons.font
+    ])
+  ]);
+}
+_handleFinishError = error => {
+  console.error(error)
+}
+_handleFinishLoading = async() => {
+  this.setState({
+    isLoadingComplete : true
+  })
+}
+
 
 const styles = StyleSheet.create({
   container: {
