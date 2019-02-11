4f9766c2ce158822a696232ac56fa10ea2defb7f
starlord <starlord@xhiftcorp.com>
2019-02-11 11:16:01 +0900

    ㅋㅏ메라 촬영 저장

--- screens/CameraScreen/index.js
+++ screens/CameraScreen/index.js
@@ -89,9 +89,23 @@ class CameraScreen extends Component{
                               )
                          }
                          <View style={styles.btnContainer}>
-                              <TouchableOpacity onPressOut={this._takePhoto}>
-                                   <View style={styles.btn}></View>
-                              </TouchableOpacity>
+                         {pictureTaken ? (
+                              <View style={styles.photoActions}>
+                                   <TouchableOpacity onPressOut={this._rejectPhoto}>
+                                        <MaterialIcons name={"cancel"} size={60} color={"black"} />
+                                   </TouchableOpacity>  
+                                   <TouchableOpacity onPressOut={this._approvePhoto}>
+                                        <MaterialIcons name={"check-circle"} size={60} color={"black"} />
+                                   </TouchableOpacity>                            
+                              </View>
+                              ) 
+                              : 
+                              (
+                                   <TouchableOpacity onPressOut={this._takePhoto}>
+                                        <View style={styles.btn}></View>
+                                   </TouchableOpacity>
+                              )}
+                              
                          </View>
                     </View>
                )
@@ -131,6 +145,21 @@ class CameraScreen extends Component{
                }
           }
      };
+
+     _rejectPhoto = () => {
+          this.setState({
+               picture:null,
+               pictureTaken:false
+          })
+     };
+     _approvePhoto = async () => {
+          const { picture } = this.state;
+          const saveResult = await CameraRoll.saveToCameraRoll(picture, "photo");
+          this.setState({
+               picture:null,
+               pictureTaken:false
+          })
+     }
 }
 
 const styles = StyleSheet.create({
@@ -161,6 +190,13 @@ const styles = StyleSheet.create({
           height:40,
           width:40,
           margin:10
+     },
+     photoActions:{
+          flexDirection:"row",
+          justifyContent:"space-around",
+          flex:1,
+          alignItems:"center",
+          width:250
      }
 
 })
